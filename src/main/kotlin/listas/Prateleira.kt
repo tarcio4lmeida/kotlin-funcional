061c0ac58e5b47dbdbe80f39e715ac557abcd0ee
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-19 15:14:04 -0300

    lidando com mutabilidade

---
+++ src/main/kotlin/listas/Prateleira.kt
@@ -0,0 +1,21 @@
+package listas
+
+data class Prateleira(
+    val genero : String,
+    val livros : MutableList<Livro>
+) {
+
+    fun organizarPorAutor() : MutableList<Livro>{
+        livros.sortBy {
+            it.autor
+        }
+        return livros
+    }
+
+    fun organizarPorAnoPublicacao() : MutableList<Livro>{
+        livros.sortBy {
+            it.anoPublicacao
+        }
+        return livros
+    }
+}
